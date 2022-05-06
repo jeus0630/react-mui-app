2a5114ac882ebbce8ba23d05b8ef649b42f8252a
jewoo <jeus0630@gmail.com>
2022-05-06 10:22:20 +0900

    전체적인 레이아웃 틀 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-function App() {  
+import { Box, Container, Stack } from "@mui/material"
+import Feed from "./components/Feed"
+import Navbar from "./components/Navbar"
+import Rightbar from "./components/Rightbar"
+import Sidebar from "./components/Sidebar"
+
+function App() {
 
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Box>
+      <Navbar></Navbar>
+      <Stack
+        direction={"row"}
+        spacing={2}
+        justifyContent='space-between'
+      >
+        <Sidebar></Sidebar>
+        <Feed></Feed>
+        <Rightbar></Rightbar>
+      </Stack>
+    </Box>
   )
 }
 
---
+++ src/components/Rightbar.tsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+
+export default function Rightbar() {
+    return (
+        <Box
+            bgcolor={'lightcoral'}
+            flex={2}
+            p={2}
+            sx={{
+                display:{
+                    xs: 'none',
+                    sm: 'block'
+                }
+            }}
+        >
+            Rightbar
+        </Box>
+    )
+}
